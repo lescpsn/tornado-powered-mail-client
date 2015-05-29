22229f6a70036ced4cf6609800b30b15d40601e9
Arnab <arnab@zsss-Mac-mini.local>
2015-05-29 19:36:28 +0600

    fixes

--- run.py
+++ run.py
@@ -1,9 +1,10 @@
 import tornado.ioloop
 import tornado.web
 # from smtp_module.send_mail import EmailHandler
-from config import HOST, USERNAME, PASSWORD
-from module.smtp.smtp_client import EmailHandler
-from module.smtp.utils import QueMail
+from config import SMTP_HOST, SMTP_USERNAME, SMTP_PASSWORD
+from module.get_mail.handler import IMAPRequestHandler
+from module.send_mail.handler import SMTPRequestHandler
+from module.send_mail.smtp_client import QueMail
 
 
 class MainHandler(tornado.web.RequestHandler):
@@ -12,12 +13,13 @@ class MainHandler(tornado.web.RequestHandler):
 
 application = tornado.web.Application([
     (r"/", MainHandler),
-    (r"/email/", EmailHandler)
+    (r"/email/", SMTPRequestHandler),
+    (r'/get_email/', IMAPRequestHandler)
 ])
 
 if __name__ == "__main__":
     qm = QueMail.get_instance()
-    qm.init(HOST, USERNAME, PASSWORD)
+    qm.init(SMTP_HOST, SMTP_USERNAME, SMTP_PASSWORD)
     qm.start()
     application.listen(9999)
     tornado.ioloop.IOLoop.instance().start()
